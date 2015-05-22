27688986326f06992cdb8bbec06033b07f29ef4d
Bryan Swagerty <bryantee@gmail.com>
2015-05-21 19:16:56 -0700

    Actual Hadouken animation
    the last commit wasn’t the whole code chunk.

--- js/app.js
+++ js/app.js
@@ -9,6 +9,29 @@ $(document).ready(function() {
 	})
 	.mousedown(function() {
 		console.log('mousedown');
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate({'left': '1020px'}, 500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+		}
+		);
+		// play hadouken sound
+		
+	})
+	.mouseup(function() {
+		console.log('mouseup');
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
 	});
 
+
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+ 	$('#hadouken-sound')[0].play();
+}
